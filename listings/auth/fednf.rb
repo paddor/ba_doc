0561bd63b8a871820af174c49524d91685af2499
Patrik Wenger <paddor@gmail.com>
2016-12-18 16:26:06 +0100

    update snippets

--- listings/auth/fednf.rb
+++ listings/auth/fednf.rb
@@ -1,19 +1,20 @@
 # load certificates
-server_cert = CZTop::Certificate.new_from(dim.nodes[:self].parent.pubkey)
-client_cert = CZTop::Certificate.load("/path/to/client_private_key")
+server_cert = CZTop::Certificate.new_from host.local_node.supernode.pubkey
+client_cert = CZTop::Certificate.load '/path/to/client_private_key'
 
 ##
 # connect the three client sockets
 #
+endpoints = host.local_node.supernode.endpoints
 
 sub = CZTop::Socket::SUB.new
-sub.CURVE_client!(client_cert, server_cert)
-sub.connect(dim.nodes[:self].parent.pub_endpoint)
+sub.CURVE_client! client_cert, server_cert
+sub.connect endpoints[:sub]
 
 push = CZTop::Socket::PUSH.new
-push.CURVE_client!(client_cert, server_cert)
-push.connect(dim.nodes[:self].parent.push_endpoint)
+push.CURVE_client! client_cert, server_cert
+push.connect endpoints[:push]
 
 dealer = CZTop::Socket::DEALER.new
-dealer.CURVE_client!(client_cert, server_cert)
-dealer.connect(dim.nodes[:self].parent.dealer_endpoint)
+dealer.CURVE_client! client_cert, server_cert
+dealer.connect endpoints[:dealer]
--- listings/auth/fedsf.rb
+++ listings/auth/fedsf.rb
@@ -1,26 +1,27 @@
 # setup client authentication
 certstore = CZTop::CertStore.new
-dim.nodes[:self].subnodes.each do |node|
-  cert = CZTop::Certificate.new_from(node.pubkey)
-  certstore.insert(cert)
+host.local_node.subnodes.each do |node|
+  cert = CZTop::Certificate.new_from node.pubkey
+  certstore.insert cert
 end
-authenticator = CZTop::Authenticator.new(certstore)
+authenticator = CZTop::Authenticator.new certstore
 
 # load private key
-server_cert = CZTop::Certificate.load("/path/to/private_key")
+server_cert = CZTop::Certificate.load '/path/to/private_key'
 
 ##
 # bind the three server sockets
 #
+endpoints = host.local_node.endpoints
 
 pub = CZTop::Socket::PUB.new
-pub.CURVE_server!(server_cert)
-pub.bind(dim.nodes[:self].pub_endpoint)
+pub.CURVE_server! server_cert
+pub.bind endpoints[:pub]
 
 pull = CZTop::Socket::PULL.new
-pull.CURVE_server!(server_cert)
-pull.bind(dim.nodes[:self].pull_endpoint)
+pull.CURVE_server! server_cert
+pull.bind endpoints[:pull]
 
 router = CZTop::Socket::ROUTER.new
-router.CURVE_server!(server_cert)
-router.bind(dim.nodes[:self].router_endpoint)
+router.CURVE_server! server_cert
+router.bind endpoints[:router]
